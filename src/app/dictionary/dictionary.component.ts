7242793f83ae8ead85452df017c8f7cc8e5e1d62
YousufIbrahim <150541735+yousufIT@users.noreply.github.com>
2024-09-08 20:06:08 +0300

    starting in the project

---
+++ src/app/dictionary/dictionary.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dictionary',
+  standalone: true,
+  imports: [],
+  templateUrl: './dictionary.component.html',
+  styleUrl: './dictionary.component.css'
+})
+export class DictionaryComponent {
+
+}
