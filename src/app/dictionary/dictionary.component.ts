b0ffaa6025008390f25b9cceeb5d070b8bc8d96d
YousufIbrahim <150541735+yousufIT@users.noreply.github.com>
2024-09-10 00:14:00 +0300

    fixing the service problem and adding  get response from the word from input to my collaborator: you should to make the html with tabs and display the data

--- src/app/dictionary/dictionary.component.ts
+++ src/app/dictionary/dictionary.component.ts
@@ -1,91 +1,55 @@
-// import { Component, OnInit } from '@angular/core';
-// import { catchError, Observable, throwError, timeout } from 'rxjs';
-// import { IDictionary } from './idictionary';
-// import { DictionaryService } from './dictionary.sevice';
-
-// @Component({
-//   selector: 'app-dictionary',
-//   standalone: true,
-//   imports: [],
-//   templateUrl: './dictionary.component.html',
-//   styleUrls: ['./dictionary.component.css']
-// })
-// export class DictionaryComponent implements OnInit {
-  
-//   dictionary$: Observable<IDictionary> | undefined;
-//   dictionary: IDictionary | undefined;
-
-//   constructor(private dictionaryService: DictionaryService) {}
-
-//   ngOnInit(): void {
-//     this.dictionary$ = this.dictionaryService.dictionary$;
-//     // الاشتراك في observable للحصول على البيانات
-//     this.dictionary$.subscribe({
-//       next: (data) => {
-//         this.dictionary = data;
-//         console.log('Dictionary data:', this.dictionary);
-//         console.log(this.dictionary.word)
-//       },
-//       error: (err) => console.error('Error fetching dictionary data:', err)
-//     });
-//   }
-// }
-import { Component, OnInit,ViewChild } from '@angular/core';
-import { catchError, Observable, throwError, timeout } from 'rxjs';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
 import { IDictionary } from './idictionary';
 import { DictionaryService } from './dictionary.sevice';
-
-
 import { TabsetComponent } from 'ngx-bootstrap/tabs';
 import { CommonModule } from '@angular/common';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dictionary',
   standalone: true,
-  imports: [CommonModule, TabsModule],
+  imports: [CommonModule, TabsModule, FormsModule],
   templateUrl: './dictionary.component.html',
   styleUrls: ['./dictionary.component.css']
 })
 export class DictionaryComponent implements OnInit {
-  
-  dictionary$: Observable<IDictionary> | undefined;
-  dictionary: IDictionary  | undefined;
 
-  word :string ="";
+  dictionary$: Observable<IDictionary> | undefined;
+  dictionary: IDictionary | undefined;
+
+  word: string = "";
 
   constructor(private dictionaryService: DictionaryService) {}
 
-  
-  ngOnInit(): void {
-    this.dictionary$ = this.dictionaryService.dictionary$;
-    // الاشتراك في observable للحصول على البيانات
-    this.dictionary$.subscribe({
-      next: (data) => {
-        this.dictionary = data;
-        console.log('Dictionary data:', this.dictionary);
-        console.log(this.dictionary.word)
-      },
-      // error: (err) => console.error('Error fetching dictionary data:', err)
-    });
-  }
-  
-  
+  ngOnInit(): void {}
+
   @ViewChild('staticTabs', { static: false }) staticTabs?: TabsetComponent;
-  
+
   disableEnable() {
     if (this.staticTabs?.tabs[2]) {
       this.staticTabs.tabs[2].disabled = !this.staticTabs.tabs[2].disabled;
     }
   }
+//this for get the data from service to component 
+  GetData(word: string): void {
+   
 
-  getData(){
-  
-if (this.dictionary) { 
-  // for (let item of this.dictionary.meanings) {
-    console.log(this.dictionary.word);
-  // }
-}
+    this.dictionary$ = this.dictionaryService.getDictionaryData(word);
+    this.dictionary$.subscribe({
+      next: (data) => {
+        this.dictionary = data;
+      },
+      error: (err) => {
+        console.error('Error fetching dictionary data:', err);
+      }
+    });
+  }
+
+  getData() {
+    if (this.dictionary) {
+      console.log(this.dictionary.word);
+    }
   }
 }
-
--- src/app/dictionary/dictionary.sevice.ts
+++ src/app/dictionary/dictionary.sevice.ts
@@ -1,36 +1,50 @@
-import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { IDictionary } from "./idictionary";
-import { catchError, throwError } from "rxjs";
-
-
-
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { IDictionary, IMeaning } from './idictionary'; 
 
 @Injectable({
-    providedIn:'root'
+  providedIn: 'root'
 })
-export class DictionaryService{
-    constructor(private http: HttpClient) { }
+export class DictionaryService {
 
-    dictionary$ = this.http.get<IDictionary>("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
-                       .pipe(
-                       catchError(this.handleError)
-                       );
-  
-    private handleError(err: HttpErrorResponse){
-      let errorMessage = '';
-  
-      if(err.error instanceof ErrorEvent){
-        // client or network error
-        errorMessage = `Error: ${err.error.message}`;
-      }
-      else {
-        // server error
-        errorMessage = `Error: ${err.message}`;
-      }
-  
-      console.error(errorMessage);
-  
-      return throwError(() => errorMessage);
-    }
+  constructor(private http: HttpClient) { }
+
+  //this for send the request with word
+  getDictionaryData(word: string): Observable<IDictionary> {
+    return this.http.get<any>(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`).pipe(
+      map(apiResponse => mapApiResponseToDictionary(apiResponse[0])) //this for mapping the response like we want in interfaces
+    );
+  }
+}
+//the mapping function actually i didn't write it alone it is so hard 
+//but i understand it :)
+function mapApiResponseToDictionary(apiResponse: any): IDictionary {
+
+  const phonetics = apiResponse.phonetics || [];
+  const phoneticText = phonetics.map((p: { text: any; }) => p.text).join(', ') || '';
+  const audioUrls = phonetics.map((p: { audio: any; }) => p.audio).filter((url: any) => url) || [];
+
+  const meanings: IMeaning[] = apiResponse.meanings.map((meaning: { partOfSpeech: any; definitions: any[]; }) => ({
+    partOfSpeech: meaning.partOfSpeech,
+    definitions: meaning.definitions.map((def: { definition: any; example: any; synonyms: any; }) => ({
+      definitionText: def.definition,
+      example: def.example || '',
+      synonyms: def.synonyms || []
+    }))
+  }));
+
+  const synonyms = apiResponse.meanings.flatMap((meaning: { synonyms: any; }) => meaning.synonyms || []);
+  const antonyms = apiResponse.meanings.flatMap((meaning: { antonyms: any; }) => meaning.antonyms || []);
+
+  const dictionary: IDictionary = {
+    word: apiResponse.word || '',
+    phonetic: phoneticText,
+    audioUrls: audioUrls,
+    meanings: meanings,
+    synonyms: synonyms,
+    antonyms: antonyms
+  };
+
+  return dictionary;
 }
